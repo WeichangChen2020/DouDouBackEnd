028d2c7361f57b3ad8c76e882521893ac13e8849
陈伟昌 <30310663+WeichangChen2020@users.noreply.github.com>
2020-02-26 17:16:02 +0800

    Update config_sae.php

--- Application/Api/Conf/config_sae.php
+++ Application/Api/Conf/config_sae.php
@@ -9,16 +9,5 @@
 return array(
 
     /* 数据库配置 */
-
-//    'DB_HOST' => 'kbvaybcarhcv.mysql.sae.sina.com.cn,lgobeqojbmjb.mysql.sae.sina.com.cn', // 服务器地址
-//    'DB_PORT' => 10059,
-//    'DB_NAME' => 'app_pingshif', // 数据库名
-//    'DB_USER' => 'admin', // 用户名
-//    'DB_PWD' =>  '123456',  // 密码
-
-
-    'DB_PREFIX' => 'pingshifen_', // 数据库表前缀
-    /* 文件上传路径 */
-    'APP_ROOT' => 'https://pingshifen.applinzi.com/', 'UPLOAD_DIR' => 'img/', 'UPLOAD_ROOT' => 'http://pingshif-img.stor.sinaapp.com/',
-
+    /* 保密文件 */
 );
