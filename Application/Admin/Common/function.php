99a0d6f926644d205a363c1eb4397070b1dbc8a9
陈伟昌 <30310663+WeichangChen2020@users.noreply.github.com>
2020-12-11 14:37:16 +0800

    Update function.php

--- Application/Admin/Common/function.php
+++ Application/Admin/Common/function.php
@@ -4,9 +4,8 @@ function email() {
 
     $mail = new \SaeMail();
 
-    $ret = $mail->quickSend("hello_lijj@foxmail.com", "这是qq发给foxmail的邮件", "lijjmake", "hello_lijj@qq.com", "hello_lijj", "smtp.qq.com", 465, TRUE); //指定smtp和端口
+    $ret = $mail->quickSend("aa@foxmail.com", "这是qq发给foxmail的邮件", "aa", "aa@qq.com", "aa", "smtp.qq.com", 465, TRUE); //指定smtp和端口
 
-//    'zvalkqmewgnyjfeh'
     //发送失败时输出错误码和错误信息
     if ($ret === false) {
         var_dump($mail->errno(), $mail->errmsg());
